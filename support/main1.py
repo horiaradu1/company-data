04f04eba614dec4b200ad276ec53deb367548299
Horia Radu <raduhoriagabriel@gmail.com>
2026-04-25 16:36:21 +0300

    clean soupp

--- support/main1.py
+++ support/main1.py
@@ -1,10 +1,36 @@
+import re
+import os
 import requests
+from bs4 import BeautifulSoup, Comment
 from concurrent.futures import ThreadPoolExecutor
-import collections
+from collections import Counter
 
-def extract_website_date(response_text: str) -> tuple:
+def clean_domain(url: str) -> str:
+    url = re.sub(r'^https?://', '', url.strip())
+    url = re.sub(r'^www\.', '', url)
+    return url.split('/')[0].split('?')[0]
+
+def clean_soup(soup: BeautifulSoup) -> BeautifulSoup:
+    # TODO: Test on all data with different attributes to see perf impact and if we actually needed some of those attributes
+    for tag in soup(["style", "script"]): # TODO: Are those all the tags?
+        tag.decompose()
+    for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
+
+    for tag in soup.find_all(True):
+        for attr in list(tag.attrs):
+            if attr.startswith(("data-", "aria-", "on")):
+                del tag[attr]
+        del tag["style"]
+
+    return soup
+
+def extract_website_data(response_soup: BeautifulSoup) -> tuple:
+    """Return: (phone_numbers, social_media_links, addresses)."""
     # TODO: Implement scraping logic
-    status = 200
+
+    
+
     return (
         [], # phone numbers
         [], # social media links
@@ -14,21 +40,25 @@ def extract_website_date(response_text: str) -> tuple:
 def process_website(website: str) -> tuple[str, int, list, list, list]:
     """Return: (website, status_code, phone_numbers, social_media_links, addresses)."""
     # TODO: Replace print with thread log
+    website = clean_domain(website)
     print(f"Processing website: {website}")
     try:
         url = website if website.startswith("https") else f"https://{website}"
         response = requests.get(url, timeout=TIMEOUT_SECONDS)
         print(f"Status code: {response.status_code}")
         if response.status_code == 200:
-            print(f"Response json: {response.json}")
-            #print(f"Response text: {response.text}")
+            # print(f"Response json: {response.json}")
+            # print(f"Response text: {response.text}")
+            soup = BeautifulSoup(response.text, "html.parser")
+            soup = clean_soup(soup)
 
             # Save response to file for testing
             # TODO: Implement response caching to avoid fetching same website
-            with open(f"responses/response_{website.replace('.', '_')}.txt", "w") as f:
-                f.write(response.text)
+            with open(f"responses/response_{website.replace('.', '_')}.html", "w") as f:
+                f.write(str(soup.prettify()))
+                # f.write(str(soup))
 
-            return (website, response.status_code, *extract_website_date(response.text))
+            return (website, response.status_code, *extract_website_data(soup))
         
         return (website, response.status_code, [], [], [])
     except Exception as e:
@@ -38,7 +68,9 @@ def process_website(website: str) -> tuple[str, int, list, list, list]:
 
     
 if __name__ == "__main__":
-    SAMPLE_WEBSITES_PATH = r"docs/Task 1 Company Data API/sample-websites.csv"
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # SAMPLE_WEBSITES_PATH = r"../docs/Task 1 Company Data API/sample-websites.csv"
+    SAMPLE_WEBSITES_PATH = os.path.join(BASE_DIR, "../docs/Task 1 Company Data API/sample-websites.csv")
     TIMEOUT_SECONDS = 5
     THREAD_POOL_WORKERS = 5
 
@@ -62,13 +94,15 @@ if __name__ == "__main__":
     print(f"Last 3 websites: {websites[-3:]}")
 
     # Check unique websites
-    websites = set(websites)
+    # websites = set(websites)
+    websites = sorted(set(websites))
     websites = list(websites)
     print(f"Unique websites: {len(websites)}")
 
     # Randomly sample 10 websites for testing
     # TODO: Remove in prod
     import random
+    random.seed(1234)
     random.shuffle(websites)
     websites = websites[:10]
     print(f"Sampled websites: {websites}")
@@ -77,7 +111,7 @@ if __name__ == "__main__":
     # TODO: Use submit instead of map to handle exceptions better and log progress?
     with ThreadPoolExecutor(max_workers=THREAD_POOL_WORKERS) as executor:
         raw_websites_data = list(executor.map(process_website, websites))
-    statuscode_counts = collections.Counter([data[1] for data in raw_websites_data])
+    statuscode_counts = Counter([data[1] for data in raw_websites_data])
     print(f"Status code counts: {statuscode_counts}")
 
     # Merge raw into structured
